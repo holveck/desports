e94b4293f2fe4d275aa92012b6a02d0eaf54f04e
holveck <brholveck@gmail.com>
2026-03-30 23:29:18 -0400

    Create sport_config.py

---
+++ utils/sport_config.py
@@ -0,0 +1,87 @@
+SPORT_CONFIG = {
+    "football": {
+        "gendered": False,
+        "classifications": [
+            "Class 3A",
+            "Class 2A",
+            "Class 1A",
+            "Division I",
+            "Division II",
+        ],
+    },
+
+    "volleyball": {
+        "gendered": True,
+        "classifications": ["Overall"],
+    },
+
+    "soccer": {
+        "gendered": True,
+        "classifications": ["Division I", "Division II", "Overall"],
+    },
+
+    "cross country": {
+        "gendered": True,
+        "classifications": ["Division I", "Division II"],
+    },
+
+    "field hockey": {
+        "gendered": False,
+        "classifications": ["Division I", "Division II", "Overall"],
+    },
+
+    "basketball": {
+        "gendered": True,
+        "classifications": ["Overall"],
+    },
+
+    "wrestling": {
+        "gendered": False,
+        "classifications": ["Division I", "Division II", "Overall"],
+        "event_type": "mixed",
+    },
+
+    "swimming and diving": {
+        "gendered": True,
+        "classifications": ["Overall"],
+        "event_type": "mixed",
+    },
+
+    "indoor track and field": {
+        "gendered": True,
+        "classifications": ["Overall"],
+        "event_type": "mixed",
+    },
+
+    "baseball": {
+        "gendered": False,
+        "classifications": ["Overall"],
+    },
+
+    "softball": {
+        "gendered": False,
+        "classifications": ["Overall"],
+    },
+
+    "lacrosse": {
+        "gendered": True,
+        "classifications": ["Overall"],
+    },
+
+    "tennis": {
+        "gendered": True,
+        "classifications": ["Overall"],
+    },
+
+    "golf": {
+        "gendered": False,
+        "classifications": ["Overall"],
+        "event_type": "mixed",
+    },
+
+    "outdoor track and field": {
+        "gendered": True,
+        "classifications": ["Division I", "Division II"],
+        "event_type": "mixed",
+    },
+}
