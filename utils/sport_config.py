0ee13c02fa4cce3d34abdc27a396e394ec0a7ba1
holveck <brholveck@gmail.com>
2026-04-01 21:01:48 -0400

    Update sport_config.py

--- utils/sport_config.py
+++ utils/sport_config.py
@@ -1,6 +1,7 @@
 SPORT_CONFIG = {
     "football": {
         "gendered": False,
+        "gender_policy": "boys_only",
         "classifications": [
             "Class 3A",
             "Class 2A",
@@ -12,75 +13,89 @@ SPORT_CONFIG = {
 
     "volleyball": {
         "gendered": True,
+        "gender_policy": "girls_only",
         "classifications": ["Overall"],
     },
 
     "soccer": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Division I", "Division II", "Overall"],
     },
 
     "cross country": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Division I", "Division II"],
     },
 
     "field hockey": {
         "gendered": False,
+        "gender_policy": "girls_only",
         "classifications": ["Division I", "Division II", "Overall"],
     },
 
     "basketball": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Overall"],
     },
 
     "wrestling": {
         "gendered": False,
+        "gender_policy": "boys_only",
         "classifications": ["Division I", "Division II", "Overall"],
         "event_type": "mixed",
     },
 
     "swimming and diving": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Overall"],
         "event_type": "mixed",
     },
 
     "indoor track and field": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Overall"],
         "event_type": "mixed",
     },
 
     "baseball": {
         "gendered": False,
+        "gender_policy": "boys_only
         "classifications": ["Overall"],
     },
 
     "softball": {
         "gendered": False,
+        "gender_policy": "girls_only",
         "classifications": ["Overall"],
     },
 
     "lacrosse": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Overall"],
     },
 
     "tennis": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Overall"],
     },
 
     "golf": {
         "gendered": False,
+        "gender_policy": "mixed",
         "classifications": ["Overall"],
         "event_type": "mixed",
     },
 
     "outdoor track and field": {
         "gendered": True,
+        "gender_policy": "gendered",
         "classifications": ["Division I", "Division II"],
         "event_type": "mixed",
     },
